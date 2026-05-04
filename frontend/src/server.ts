04331789f9339b824a6c6cc782025f41f437517d
NijucoOriginal <diegonicolaspenarincon@gmail.com>
2026-05-04 14:57:05 -0500

    Se modifico el archivo server.ts

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -10,7 +10,11 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: process.env['ALLOWED_HOSTS']?.split(',') ?? ['localhost']
+});
+
+
 
 /**
  * Example Express Rest API endpoints can be defined here.
